c87a2fbc387c49614dcb843279cca4b1e74daec9
Chris Hilditch <christopher.hilditch@gmail.com>
2025-08-24 15:10:05 +0100

    Add 404 & vercel hosting setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Pitch } from "./components/pages/tuner/page";
 import { Route, Routes } from "react-router";
 import HowToUse from "./components/pages/howToUse/page";
 import About from "./components/pages/about/page";
+import NotFound from "./components/pages/notFound/page";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route index element={<Pitch />} />
         <Route path="how-to-use" element={<HowToUse />} />
         <Route path="about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
   );
---
+++ src/components/pages/notFound/page.tsx
@@ -0,0 +1,13 @@
+import { H1 } from "../../designSystem/headers";
+import { P } from "../../designSystem/p";
+
+const About = () => {
+  return (
+    <div className="flex flex-col items-center justify-center  p-6">
+      <H1>Page not found</H1>
+      <P>This page doesn't exist on this website.</P>
+    </div>
+  );
+};
+
+export default About;
